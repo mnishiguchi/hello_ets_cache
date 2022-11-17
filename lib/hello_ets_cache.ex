04ceedc58e8ddc5da7e41320a3e7d83593d49f13
Masatoshi N <nishiguchi.masa@gmail.com>
2022-11-16 20:56:59 -0500

    Improve HelloEtsCache

--- lib/hello_ets_cache.ex
+++ lib/hello_ets_cache.ex
@@ -1,114 +1,129 @@
 defmodule HelloEtsCache do
-  @moduledoc false
-
   use GenServer
 
+  require Ex2ms
+
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts, name: opts[:name])
+    server_name = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, opts, name: server_name)
   end
 
-  @spec get(GenServer.name(), any()) :: any()
-  def get(server_name, key, default \\ nil) do
-    GenServer.call(server_name, {:get, key, default})
+  def get(cache_name, key, default \\ nil) do
+    GenServer.call(cache_name, {:get, key, default})
   end
 
-  @spec put(GenServer.name(), any(), any()) :: :ok
-  def put(server_name, key, value) do
-    GenServer.cast(server_name, {:put, key, value})
+  def put(cache_name, key, value) do
+    GenServer.cast(cache_name, {:put, key, value})
   end
 
-  @spec delete_all(GenServer.name()) :: :ok
-  def delete_all(server_name) do
-    GenServer.cast(server_name, :delete_all)
+  def delete_all(cache_name) do
+    GenServer.cast(cache_name, :delete_all)
   end
 
-  @impl GenServer
+  def stop(cache_name) do
+    GenServer.stop(cache_name)
+  end
+
+  def entries(cache_name) do
+    :ets.select(
+      cache_name,
+      Ex2ms.fun do
+        {k, v, _ttl} -> {k, v}
+      end
+    )
+  end
+
+  @impl true
   def init(args) do
-    ets_args = args[:ets_args] || []
     cache_name = args[:name]
     cache_ttl = args[:ttl] || :infinity
-    get_time_ms = args[:get_time_ms] || fn -> System.monotonic_time(:millisecond) end
+    cleanup_interval = args[:cleanup_interval] || :timer.minutes(60)
 
-    ^cache_name = :ets.new(cache_name, [:set, :named_table, :public] ++ ets_args)
+    # ETSテーブルを生成
+    ^cache_name = :ets.new(cache_name, [:set, :named_table, :public])
 
+    # キャッシュの名前とTTLを覚えておく
     state = %{
       cache_name: cache_name,
       cache_ttl: cache_ttl,
-      get_time_ms: get_time_ms
+      cleanup_interval: cleanup_interval
     }
 
-    {:ok, state}
+    {:ok, state, {:continue, :after_init}}
   end
 
-  @impl GenServer
-  def handle_call({:get, key, default}, _from, state) do
-    time_now_ms = state.get_time_ms.()
-
-    case :ets.lookup(state.cache_name, key) do
-      [] ->
-        :telemetry.execute(
-          [__MODULE__, :get],
-          %{status: :miss},
-          %{key: key, cache: state.cache_name}
-        )
-
-        {:reply, default, state}
-
-      [{^key, _value, ts, ttl}] when is_integer(ttl) and ts + ttl <= time_now_ms ->
-        :telemetry.execute(
-          [__MODULE__, :get],
-          %{status: :miss},
-          %{key: key, cache: state.cache_name}
-        )
-
-        {:reply, default, state}
-
-      [{^key, value, _ts, _expire_at}] ->
-        :telemetry.execute(
-          [__MODULE__, :get],
-          %{status: :hit},
-          %{key: key, cache: state.cache_name}
-        )
-
-        {:reply, value, state}
-    end
-  end
-
-  @impl GenServer
-  def handle_cast({:put, key, value}, state) do
-    :telemetry.execute([__MODULE__, :put], %{}, %{key: key, cache: state.cache_name})
-
-    time_now_ms = state.get_time_ms.()
-
-    if state.cache_ttl < 0 do
-      raise ArgumentError, "`:ttl` must be greater than 0"
-    end
-
-    true = :ets.insert(state.cache_name, {key, value, time_now_ms, state.cache_ttl})
+  @impl true
+  def handle_continue(:after_init, state) do
+    Process.send_after(self(), :delete_expired, state.cleanup_interval)
 
     {:noreply, state}
   end
 
-  @impl GenServer
+  @impl true
+  def handle_call({:get, key, default}, _from, state) do
+    time_now_ms = System.monotonic_time(:millisecond)
+
+    reply =
+      case :ets.lookup(state.cache_name, key) do
+        # 値が見つからなかった場合、デフォルトの値を返す
+        [] ->
+          default
+
+        # 値が見つかったが期限切れの場合、デフォルトの値を返す
+        [{^key, _value, inserted_at_ms}]
+        when is_integer(state.cache_ttl) and inserted_at_ms + state.cache_ttl <= time_now_ms ->
+          default
+
+        # 見つかった値を返す
+        [{^key, value, _inserted_at_ms}] ->
+          value
+      end
+
+    {:reply, reply, state}
+  end
+
+  @impl true
+  def handle_cast({:put, key, value}, state) do
+    inserted_at_ms = System.monotonic_time(:millisecond)
+
+    # キーと値のペアを時刻（マイクロ秒）と共に挿入
+    true = :ets.insert(state.cache_name, {key, value, inserted_at_ms})
+
+    {:noreply, state}
+  end
+
+  @impl true
   def handle_cast(:delete_all, state) do
     :ets.delete_all_objects(state.cache_name)
 
     {:noreply, state}
   end
 
-  @impl GenServer
-  def handle_cast(:delete_expired, state) do
-    time_now_ms = state.get_time_ms.()
+  @impl true
+  def handle_info(:delete_expired, state) do
+    Process.send_after(self(), :delete_expired, state.cleanup_interval)
 
-    # See https://github.com/elixir-toniq/mentat/blob/7f1811779ca2dfc80dcb30fe5d70d5809afb3abb/lib/mentat.ex#L228
-    match_spec =
-      :ets.fun2ms(fn {_key, _value, inserted_at_ms, ttl_ms}
-                     when is_integer(ttl_ms) and inserted_at_ms + ttl_ms < time_now_ms ->
-        true
-      end)
-
-    :ets.select_delete(state.cache_name, match_spec)
+    do_delete_expired(state)
 
     {:noreply, state}
+  end
+
+  @impl true
+  def terminate(reason, state) do
+    :ets.delete(state.cache_name)
+    reason
+  end
+
+  defp do_delete_expired(%{cache_name: cache_name, cache_ttl: cache_ttl}) do
+    time_now_ms = System.monotonic_time(:millisecond)
+
+    :ets.select_delete(
+      cache_name,
+      Ex2ms.fun do
+        {_key, _value, inserted_at_ms}
+        when is_integer(^cache_ttl) and inserted_at_ms + ^cache_ttl < ^time_now_ms ->
+          true
+      end
+    )
   end
 end
--- test/hello_ets_cache_test.exs
+++ test/hello_ets_cache_test.exs
@@ -2,16 +2,73 @@ defmodule HelloEtsCacheTest do
   use ExUnit.Case
   doctest HelloEtsCache
 
-  test "TODO" do
-    HelloEtsCache.start_link(name: :test_ets, ttl: 1_000)
+  test "cache for 1 second" do
+    {:ok, _} = HelloEtsCache.start_link(name: :test_ets, ttl: 500)
 
     assert HelloEtsCache.get(:test_ets, :a) |> is_nil()
 
     HelloEtsCache.put(:test_ets, :a, 1)
     assert HelloEtsCache.get(:test_ets, :a) == 1
 
-    Process.sleep(1000)
+    Process.sleep(500)
 
     assert HelloEtsCache.get(:test_ets, :a) |> is_nil()
+  end
+
+  test "cache forever" do
+    {:ok, _} = HelloEtsCache.start_link(name: :test_ets, ttl: :infinity)
+
+    HelloEtsCache.put(:test_ets, :a, 1)
+    assert HelloEtsCache.get(:test_ets, :a) == 1
+
+    Process.sleep(500)
+
+    assert HelloEtsCache.get(:test_ets, :a) == 1
+  end
+
+  test "delete_all" do
+    {:ok, _} = HelloEtsCache.start_link(name: :test_ets, ttl: :infinity)
+
+    HelloEtsCache.put(:test_ets, :a, 1)
+    HelloEtsCache.put(:test_ets, :b, 2)
+    HelloEtsCache.put(:test_ets, :c, 3)
+
+    assert HelloEtsCache.get(:test_ets, :a) == 1
+    assert HelloEtsCache.get(:test_ets, :b) == 2
+    assert HelloEtsCache.get(:test_ets, :c) == 3
+
+    HelloEtsCache.delete_all(:test_ets)
+
+    assert HelloEtsCache.get(:test_ets, :a) |> is_nil()
+    assert HelloEtsCache.get(:test_ets, :b) |> is_nil()
+    assert HelloEtsCache.get(:test_ets, :c) |> is_nil()
+  end
+
+  test "entries" do
+    {:ok, _} = HelloEtsCache.start_link(name: :test_ets, ttl: 3, cleanup_interval: 500)
+
+    # Insert some records
+    HelloEtsCache.put(:test_ets, :a, 1)
+    HelloEtsCache.put(:test_ets, :b, 2)
+    HelloEtsCache.put(:test_ets, :c, 3)
+
+    # The records are accessible before expired
+    assert HelloEtsCache.get(:test_ets, :a) == 1
+    assert HelloEtsCache.get(:test_ets, :b) == 2
+    assert HelloEtsCache.get(:test_ets, :c) == 3
+    assert [{:c, 3}, {:b, 2}, {:a, 1}] == HelloEtsCache.entries(:test_ets)
+
+    Process.sleep(3)
+
+    # The records are not accessible after expired
+    assert HelloEtsCache.get(:test_ets, :a) |> is_nil()
+    assert HelloEtsCache.get(:test_ets, :b) |> is_nil()
+    assert HelloEtsCache.get(:test_ets, :c) |> is_nil()
+    assert [{:c, 3}, {:b, 2}, {:a, 1}] == HelloEtsCache.entries(:test_ets)
+
+    Process.sleep(500)
+
+    # Internal entries gets deleted after cleanup interval elapsed
+    assert HelloEtsCache.entries(:test_ets) |> Enum.empty?()
   end
 end
